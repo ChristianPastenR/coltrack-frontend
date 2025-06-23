431dac8f613c93168ce42c353d764891de812f32
Christian Pasten <christian.pasten87@gmail.com>
2025-06-23 13:45:24 -0400

    Se cambia a ES y color de marcador a rojo

--- src/App.js
+++ src/App.js
@@ -22,15 +22,14 @@ const copiapoBounds = [
 ];
 
 const userIcon = new L.Icon({
-  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-  iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-  className: 'user-location-icon'
+  shadowSize: [41, 41]
 });
+
 
 
 function MoveToLocation({ position }) {
